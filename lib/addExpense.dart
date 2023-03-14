fb181114bed8e02184d6dd6dd68abc866080a66d
Davide Teixeira <davidpcu@hotmail.com>
2023-03-14 21:42:13 +0000

    Addes some more widgets

--- lib/addExpense.dart
+++ lib/addExpense.dart
@@ -16,11 +16,20 @@ class _addExpense extends State<addExpense> {
 
   @override
   Widget build(BuildContext context) {
+    bool _showInputBoxTable = false;
     String dropdownvalue = '€';
     var items = [
       "€",
       "\$",
       "£",
+    ];
+
+    String category = 'Leisure';
+    var categories = [
+      "Leisure",
+      "Food",
+      "Transport",
+      "Education",
     ];
     return Scaffold(
       appBar: AppBar(
@@ -32,6 +41,7 @@ class _addExpense extends State<addExpense> {
             alignment: Alignment(-0.5, -0.7),
              child: DropdownButton<String>(
                value: dropdownvalue,
+                 underline: SizedBox(),
 
                icon: const Icon(Icons.keyboard_arrow_down),
 
@@ -50,9 +60,64 @@ class _addExpense extends State<addExpense> {
 
 
             )
-          )
+          ),
 
-          ,Align(
+          Align(
+            alignment: Alignment(0.3, -0.7),
+            child: SizedBox(
+                width: 100,
+              //height: 10,
+              child: TextField(
+                decoration: new InputDecoration(),
+                keyboardType: TextInputType.number,
+
+                )
+            ),
+            ),
+
+          Align(
+              alignment: Alignment(0, -0.35),
+              child: DropdownButton<String>(
+                value: category,
+                underline: SizedBox(),
+
+                icon: const Icon(Icons.keyboard_arrow_down),
+
+                onChanged: (newValue) async{
+                  setState(() {
+                    dropdownvalue = newValue!;
+                  });
+                },
+
+                items: categories.map((String categories){
+                  return DropdownMenuItem<String>(
+                    value: categories,
+                    child: Text(categories),
+                  );
+                }).toList(),
+
+
+              )
+          ),
+
+          Align(
+            alignment: Alignment(0, -0.1),
+              child: RadioListTile(
+                  title: Text("Recurring Expense"),
+                  value: "radio value",
+                  groupValue: "",
+                  onChanged: (value) {
+                    setState(() {
+
+                      _showInputBoxTable = true;
+                    }
+                    );
+                  }),
+              ),
+
+
+
+          Align(
             //alignment: Alignment(50, 50),
           alignment: Alignment(0, 0.9),
 
