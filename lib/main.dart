3bb21ec7477abe150c700eac5d02a0c8ca8e9196
Davide Teixeira <davidpcu@hotmail.com>
2023-03-27 14:38:58 +0100

    Synchroned to the database

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'Model/User.dart';
 import 'View/HomePageView.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitUp]);
 
-  runApp(const MyApp());
+  //WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+
+// Check if Firestore is initialized
+  if (FirebaseFirestore.instance.settings == null) {
+    print('Firestore is not yet initialized.');
+  } else {
+    print('Firestore is already initialized.');
+  }
+
+  DocumentReference docRef = FirebaseFirestore.instance.collection('Users').doc('argQUZxJx7xHNtPKfl4N');
+
+  // Get the document data
+  DocumentSnapshot snapshot = await docRef.get();
+  if (snapshot.exists) {
+    // Print the document data
+    print(snapshot.data());
+  } else {
+    print('Document does not exist');
+  }
+
+  runApp(MyApp());
+
 }
 
 
