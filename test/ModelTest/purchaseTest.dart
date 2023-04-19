a0eca0a64aae0bce3e3b419c5133d7996417bf5f
Máximo <up202108887@up.pt>
2023-04-19 19:56:37 +0100

    added tests to constraints in purchaseTest

--- test/ModelTest/purchaseTest.dart
+++ test/ModelTest/purchaseTest.dart
@@ -1,76 +1,56 @@
 import 'package:MoneyWatch/Model/Purchase.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-
   group('Purchase', () {
-    test('Purchase constructor sets all attributes', () {
-      final purchase = Purchase(
-        10.0,
-        'Description',
-        'Category',
-        5,
-        DateTime.now(),
-      );
-      expect(purchase.amount, equals(10.0));
-      expect(purchase.description, equals('Description'));
-      expect(purchase.category, equals('Category'));
-      expect(purchase.nr_days, equals(5));
-      expect(purchase.datetime, isInstanceOf<DateTime>());
+    test('Purchase constructor throws error if amount is negative', () {
+      expect(() => Purchase(-1.0, 'test', 'test', 1, DateTime.now()),
+          throwsArgumentError);
     });
 
-    test('compareTo compares purchases by date and amount', () {
-      final purchase1 = Purchase(
-        10.0,
-        'Description',
-        'Category',
-        5,
-        DateTime(2022, 4, 19),
-      );
-      final purchase2 = Purchase(
-        20.0,
-        'Another description',
-        'Another category',
-        0,
-        DateTime(2022, 4, 20),
-      );
-      final result = purchase1.compareTo(purchase2);
-      expect(result, lessThan(0));
+    test('Purchase constructor throws error if nr_days is negative', () {
+      expect(() => Purchase(1.0, 'test', 'test', -1, DateTime.now()),
+          throwsArgumentError);
     });
 
-    test('compareTo compares purchases by amount if dates are equal', () {
-      final purchase1 = Purchase(
-        10.0,
-        'Description',
-        'Category',
-        5,
-        DateTime.now(),
-      );
-      final purchase2 = Purchase(
-        20.0,
-        'Another description',
-        'Another category',
-        0,
-        DateTime.now(),
-      );
-      final result = purchase1.compareTo(purchase2);
-      expect(result, lessThan(0));
+    test('Purchase constructor throws error if category is empty', () {
+      expect(() => Purchase(1.0, 'test', '', 1, DateTime.now()),
+          throwsArgumentError);
     });
 
-    test('setAttributes sets all attributes', () {
-      final purchase = Purchase(
-        10.0,
-        'Description',
-        'Category',
-        5,
-        DateTime.now(),
-      );
-      purchase.setAttributes(20.0, 'New description', 'New category', 10);
+    test('Purchase compareTo function works correctly', () {
+      var purchase1 = Purchase(10.0, 'test1', 'category', 1, DateTime.now());
+      var purchase2 = Purchase(20.0, 'test2', 'category', 1, DateTime.now());
+      expect(purchase1.compareTo(purchase2), lessThan(0));
+      expect(purchase2.compareTo(purchase1), greaterThan(0));
+      expect(purchase1.compareTo(purchase1), equals(0));
+    });
+
+    test('setAttributes function works correctly', () {
+      var purchase = Purchase(10.0, 'test1', 'category', 1, DateTime.now());
+      purchase.setAttributes(20.0, 'test2', 'category2', 2);
       expect(purchase.amount, equals(20.0));
-      expect(purchase.description, equals('New description'));
-      expect(purchase.category, equals('New category'));
-      expect(purchase.nr_days, equals(10));
+      expect(purchase.description, equals('test2'));
+      expect(purchase.category, equals('category2'));
+      expect(purchase.nr_days, equals(2));
+    });
+
+    test('setAttributes function throws error if amount is negative', () {
+      var purchase = Purchase(10.0, 'test1', 'category', 1, DateTime.now());
+      expect(() => purchase.setAttributes(-1.0, 'test', 'category', 1),
+          throwsArgumentError);
+    });
+
+    test('setAttributes function throws error if nr_days is negative', () {
+      var purchase = Purchase(10.0, 'test1', 'category', 1, DateTime.now());
+      expect(() => purchase.setAttributes(1.0, 'test', 'category', -1),
+          throwsArgumentError);
+    });
+
+    test('setAttributes function throws error if category is empty', () {
+      var purchase = Purchase(10.0, 'test1', 'category', 1, DateTime.now());
+      expect(() => purchase.setAttributes(1.0, 'test', '', 1),
+          throwsArgumentError);
     });
   });
 }
