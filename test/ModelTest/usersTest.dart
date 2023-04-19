f027edba52f53cc9eaa2475efc0538e9102ed730
Máximo <up202108887@up.pt>
2023-04-19 19:53:39 +0100

    added constructor tests in userTest

--- test/ModelTest/usersTest.dart
+++ test/ModelTest/usersTest.dart
@@ -1,3 +1,5 @@
+import 'package:MoneyWatch/Model/Habit.dart';
+import 'package:MoneyWatch/Model/Post.dart';
 import 'package:MoneyWatch/Model/Purchase.dart';
 import 'package:MoneyWatch/Model/User.dart';
 import 'package:MoneyWatch/View/StatisticsPageView.dart';
@@ -13,6 +15,54 @@ class MockDocumentSnapshot extends Mock implements DocumentSnapshot {}
 
 void main() {
   group('User', () {
+
+        test('constructor sets properties correctly', () {
+          // Arrange
+          const id = '12345';
+          const username = 'TestUser';
+          final habits = <Habit>[];
+          final posts = <Post>[];
+          final customCategories = <String>[];
+          final purchases = <Purchase>[];
+
+          // Act
+          final user = User(id: id, username: username, habits: habits, posts: posts, customCategories: customCategories, purchases: purchases);
+
+          // Assert
+          expect(user.id, equals(id));
+          expect(user.username, equals(username));
+          expect(user.habits, equals(habits));
+          expect(user.posts, equals(posts));
+          expect(user.customCategories, equals(customCategories));
+          expect(user.purchases, equals(purchases));
+        });
+
+        test('id is not empty', () {
+          // Arrange
+          const id = '';
+          const username = 'TestUser';
+          final habits = <Habit>[];
+          final posts = <Post>[];
+          final customCategories = <String>[];
+          final purchases = <Purchase>[];
+
+          // Act and Assert
+          expect(() => User(id: id, username: username, habits: habits, posts: posts, customCategories: customCategories, purchases: purchases), throwsArgumentError);
+        });
+
+        test('username is not empty', () {
+          // Arrange
+          const id = '12345';
+          const username = '';
+          final habits = <Habit>[];
+          final posts = <Post>[];
+          final customCategories = <String>[];
+          final purchases = <Purchase>[];
+
+          // Act and Assert
+          expect(() => User(id: id, username: username, habits: habits, posts: posts, customCategories: customCategories, purchases: purchases), throwsArgumentError);
+        });
+
 /*
     test('addExpenses() should add a purchase to user', () {
       User user = User(
