d68ca7f9e68ca88ffb1cf05ae31eafc32edfc19f
Davide Teixeira <davidpcu@hotmail.com>
2023-05-26 08:58:33 +0100

    the unit tests are functional

--- test/ModelTest/habitTest.dart
+++ test/ModelTest/habitTest.dart
@@ -4,12 +4,13 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Habit', () {
     test('constructor initializes fields correctly', () {
+      const id= 'id';
       const name = 'Coffee';
       const description = 'I drink coffee twice a day';
       final startDate = DateTime.now();
       const amountPerDay = 2.0;
 
-      final habit = Habit(name, description, startDate, amountPerDay as double);
+      final habit = Habit(id, name, description, startDate, amountPerDay as double);
 
       expect(habit.name, equals(name));
       expect(habit.description, equals(description));
